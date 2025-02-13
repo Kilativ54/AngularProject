6b0d21c1f7c65e876806aa7ed7556b5f69e50afc
Kilativ54 <vitalikechkalov@gmail.com>
2025-02-13 11:00:04 +0100

    test work with style of component

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './test.component.html',
-  styleUrl: './test.component.scss'
+  styleUrl: './test.component.scss',
 })
 export class TestComponent {
+  title = 'title works!';
+  firstName = 'Vitalii';
+  lastName = 'Yechkalov!';
+  isEnabled = false;
 
+  isActive: boolean = true;
+  isDisabled: boolean = false;
+
+  getFullName() {
+    return `My namae is ${this.firstName} ${this.lastName}`;
+  }
 }
